b39842210828f8c3c4698eef2c530dd5f78d044b
FrancieleDalarosa <francidalaro@gmail.com>
2023-08-02 09:58:15 -0300

    feat: struct event

---
+++ evento.h
@@ -0,0 +1,19 @@
+#ifndef EVENTO_H
+#define EVENTO_H
+
+#include "data.h"
+#include "hora.h"
+
+struct node{
+    char descricao[40];
+    date data;
+    hour horas;
+    hour duracao;
+};
+
+typedef struct node evento;
+
+void fillEvent(evento *event);
+void showEvent(evento *event);
+
+#endif
